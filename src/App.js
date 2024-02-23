497f3e3d269ac5181a389b07b0e307e9f14c74ea
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2024-02-23 20:20:47 +0000

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,29 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { useEffect } from 'react';
+import Header from './component/header/Header';
+import Home from './component/home/Home';
+import Checkout from './component/checkout/Checkout';
+import Login from './component/auth/Login';
+import { auth } from './firebase';
+import { useStateValue } from './component/context';
 
 function App() {
+  const [, dispatch] = useStateValue();
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      console.log('The user is >>>>', authUser);
+      if (authUser) {
+        dispatch({
+          type: 'SET_USER',
+          user: authUser,
+        });
+      } else {
+        dispatch({
+          type: 'SET_USER',
+          user: authUser,
+        });
+      }
+    });
+  }, [dispatch]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="app">
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route
+            path="/*"
+            element={(
+              <>
+                <Header />
+                <Routes>
+
+                  <Route path="/" element={<Home />} />
+                  <Route path="/checkout" element={<Checkout />} />
+
+                </Routes>
+              </>
+        )}
+          />
+        </Routes>
+
+      </div>
+    </BrowserRouter>
   );
 }
-
 export default App;
