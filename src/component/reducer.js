395b17e5a410e2bdd4eca6421a2845f14cce9eef
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2024-02-23 20:15:27 +0000

    Add reducer js file

---
+++ src/component/reducer.js
@@ -0,0 +1,36 @@
+export const initialState = {
+  basket: [],
+  user: null,
+};
+export const getBasketTotal = (basket) => basket?.reduce((amount, item) => item.price + amount, 0);
+
+export const reducer = (state, action) => {
+  const indexToRemove = state.basket.findIndex((item) => item.id === action.id);
+
+  switch (action.type) {
+    case 'ADD_TO_BASKET':
+      return {
+        ...state,
+        basket: [...state.basket, action.item],
+      };
+
+    case 'REMOVE_FROM_BASKET':
+
+      if (indexToRemove !== -1) {
+        const newBasket = [...state.basket];
+        newBasket.splice(indexToRemove, 1);
+        return {
+          ...state,
+          basket: newBasket,
+        };
+      }
+      return state;
+    case 'SET_USER':
+      return {
+        ...state,
+        user: action.user,
+      };
+    default:
+      return state;
+  }
+};
