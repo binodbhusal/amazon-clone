d499bad6be047980adcd457eb41f08d66667a3e6
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2024-03-05 20:00:22 +0000

    refactor header component

--- src/component/header/Header.jsx
+++ src/component/header/Header.jsx
@@ -1,54 +1,140 @@
 import React from 'react';
-import './Header.scss';
-import { IoMdSearch } from 'react-icons/io';
+import { useDispatch, useSelector } from 'react-redux';
+
 import { PiShoppingCartSimple } from 'react-icons/pi';
-import { Link } from 'react-router-dom';
-import { useStateValue } from '../context';
+import { Link, useNavigate } from 'react-router-dom';
+import { LuMenu } from 'react-icons/lu';
+import { GrLocation } from 'react-icons/gr';
+
 import { auth } from '../../firebase';
+import Search from './Search';
+import { setUser } from '../../Redux/cartSlice';
 
 const Header = () => {
-  const [{ basket, user }] = useStateValue();
+  const { productNumber } = useSelector((state) => state.cart);
+  const user = useSelector((state) => state.cart.user);
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
   const handleAuth = () => {
     if (user) {
-      auth.signOut();
+      auth
+        .signOut()
+        .then(() => {
+          dispatch(setUser({ user: null }));
+          navigate('/login');
+        });
     }
   };
   return (
-    <div className="header">
-      <Link to="/">
-        <img src="./amazon-logo.webp" alt="amazon-logo" className="amazon-logo" />
-      </Link>
-      <div className="search-bar">
-        <input type="text" />
-        <IoMdSearch className="search-icon" />
-      </div>
-      <div className="header-navbar">
-        <Link to={!user && '/login'}>
-          <button type="button" onClick={handleAuth} className="menu-options">
-            <span className="text-menu-option-one">
-              Hello,
-              {' '}
-              {!user ? 'Guest' : user.email}
-            </span>
-            <span className="text-menu-option-two">{user ? 'Signout' : 'Sign in'}</span>
-          </button>
-        </Link>
-        <div className="menu-options">
-          <span className="text-menu-option-one">Returns</span>
-          <span className="text-menu-option-two">&Orders</span>
-        </div>
-        <div className="menu-options">
-          <span className="text-menu-option-one">Your</span>
-          <span className="text-menu-option-two">Prime</span>
-        </div>
-        <div className="shoping-cart">
-          <Link to="/checkout">
-            <PiShoppingCartSimple style={{ fontSize: '25px', cursor: 'pointer', color: '#fff' }} />
+    <div className="min-w[1000px]">
+      <div className="flex bg-amazonclone text-white h-[60px]">
+        {/* left section */}
+        <div className="flex items-center">
+          <Link to="/">
+            <div className="border border-transparent m-1 py-2 px-2 mr-4  hover:border-white">
+              <img
+                className="h-[35px] w-[100px]"
+                src="./amazon-logo.webp"
+                alt="amazon-logo"
+              />
+            </div>
           </Link>
-          <span className="text-menu-option-two cart-ount">{basket?.length}</span>
+          <div className="border border-transparent py-1 px-2 pl-6 hover:border-white">
+            <div className="text-xs lg:text-sm"><span className="text-gray-300 text-xs font-semibold font-Amazon-Ember-Regular">Deliver to</span></div>
+            <div className=" flex text-sm lg:text-base mt-[-5px]">
+              <GrLocation className=" ml-[-20px] text-[20px]" />
+              <span className="font-bold">Portugal</span>
+            </div>
+          </div>
         </div>
+        {/* center section */}
+        <div className="flex-grow flex items-center mx-3">
+          <Search />
+        </div>
+        {/* right section  */}
+        <div className="flex items-center">
+          <div className="border border-transparent  py-1 px-2  hover:border-white">
+            <div className="text-xs lg:text-sm">
+              <Link to={!user && '/login'}>
+                <button type="button" onClick={handleAuth} className="menu-options">
+                  <span className="text-menu-option-one">
+                    Hello,
+                    {' '}
+                    {!user ? 'Guest' : user.email}
+                  </span>
+                  <div className="text-sm lg:text-base mt[-2] font-semibold">{user ? 'Signout' : 'Sign in'}</div>
+                </button>
+              </Link>
+            </div>
+
+          </div>
+          <div className="border border-transparent  py-1 px-2  hover:border-white">
+            <Link to={user ? '/orders' : '/login'}>
+              <div className="text-xs lg:text-sm">Returns</div>
+              <div className="text-sm lg:text-base mt[-2] font-semibold">& Orders</div>
+            </Link>
+          </div>
+          <div className="pr-3 pl-2 flex border border-transparent py-1 hover:border-white">
+            <Link to="/cart">
+              <PiShoppingCartSimple style={{ fontSize: '45px', color: '#fff' }} />
+              <div className="relative text-white"><span className="absolute text-orange-400 font-bold mt-[-35px] right-[15px]">{productNumber}</span></div>
+            </Link>
+            <div className="text-sm lg:text-base mt-5 font-semibold">Cart</div>
+          </div>
+        </div>
+      </div>
+      <div className="flex items-center text-xs font-semibold lg:text-sm space-x-4 bg-amazonclone-light_blue p-2 text-white">
+        <div className="flex text-base">
+
+          <LuMenu style={{ fontSize: '25px' }} />
+          <span>
+            All
+          </span>
+
+        </div>
+
+        <div>Today&apos;s Deals</div>
+        <div>Customer Service</div>
+        <div>Registry</div>
+        <div>Gift Cards</div>
+        <div>Sell</div>
       </div>
     </div>
+  //     <Link to="/">
+  //     </Link>
+  //     <div className="search-bar">
+  //       <input type="text" />
+  //       <IoMdSearch className="search-icon" />
+  //     </div>
+  //     <div className="header-navbar">
+  //       <Link to={!user && '/login'}>
+  //         <button type="button" onClick={handleAuth} className="menu-options">
+  //           <span className="text-menu-option-one">
+  //             Hello,
+  //             {' '}
+  //             {!user ? 'Guest' : user.email}
+  //           </span>
+  //           <span className="text-menu-option-two">{user ? 'Signout' : 'Sign in'}</span>
+  //         </button>
+  //       </Link>
+  //       <div className="menu-options">
+  //         <span className="text-menu-option-one">Returns</span>
+  //         <span className="text-menu-option-two">&Orders</span>
+  //       </div>
+  //       <div className="menu-options">
+  //         <span className="text-menu-option-one">Your</span>
+  //         <span className="text-menu-option-two">Prime</span>
+  //       </div>
+  //       <div className="shoping-cart">
+  //         <Link to="/checkout">
+  // eslint-disable-next-line max-len
+  //         </Link>
+  //         <span className="text-menu-option-two cart-ount">{basket?.length}</span>
+  //       </div>
+  //     </div>
+  //   </div>
+  // );
   );
 };
 export default Header;
