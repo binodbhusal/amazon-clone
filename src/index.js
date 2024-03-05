40e672bca6f1ee94f4b9cf457219b7aee6b32fb1
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2024-03-05 19:56:03 +0000

    update index.js add redux store

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { Provider } from 'react-redux';
 import App from './App';
-import { ContextProvider } from './component/context';
-import { initialState, reducer } from './component/reducer';
+// import { ContextProvider } from './component/context';
+// import { initialState, reducer } from './component/reducer';
+import store from './Redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ContextProvider initialState={initialState} reducer={reducer}>
+    {/* <ContextProvider initialState={initialState} reducer={reducer}> */}
+    <Provider store={store}>
       <App />
-    </ContextProvider>
+    </Provider>
+    {/* </ContextProvider> */}
   </React.StrictMode>,
 );
