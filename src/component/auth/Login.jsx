f19ede3d5f943b0ec988180306f9fece0cdce6b1
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2024-03-10 12:34:43 +0000

    Update and refactor login code

--- src/component/auth/Login.jsx
+++ src/component/auth/Login.jsx
@@ -1,7 +1,7 @@
 import { Link, useNavigate } from 'react-router-dom';
 import './Login.scss';
 import { useState } from 'react';
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
+import { signInWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../../firebase';
 
 const Login = () => {
@@ -22,42 +22,77 @@ const Login = () => {
       .catch((error) => {
         const errorCode = error.code;
         const errorMessage = error.message;
-        console.log(errorCode, errorMessage);
+        throw Error(errorCode, errorMessage);
       });
   };
-  const handleSignup = (e) => {
-    e.preventDefault();
-    createUserWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
-        // Signed up
-        const { user } = userCredential;
-        if (user) {
-          navigate('/');
-        }
-        console.log(user); // Logging the user, not the auth module
-      })
-      .catch((error) => alert(error.message));
+  const handleSignup = () => {
+    navigate('/Signup');
   };
 
   return (
-    <div className="login-page">
-      <Link to="/">
-        <img className="amazon-logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1206px-Amazon_logo.svg.png" alt="amazon-logo" />
-      </Link>
-      <div className="login-container">
-        <h1>Sign in</h1>
-        <form>
-          <h5>Email</h5>
-          <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
-          <h5>Password</h5>
-          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-          <button type="submit" className="btn-login" onClick={handleLogin}>Sign in</button>
+    <>
+      <div className="pt-6 flex flex-col items-center justify-center bg-gray-100">
+        <Link to="/">
+          <img className="onject-contain w-20 mb-4" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1206px-Amazon_logo.svg.png" alt="amazon-logo" />
+        </Link>
+        <div className="flex flex-col bg-transparent  px-4 sm:px-6 md:px-8 lg:px-6 py-6 rounded-md w-full max-w-sm border border-gray-300">
 
-        </form>
-        <p>By continuing, you agree to FAKE AMAZON CLONE Conditions of Use and Privacy Notice.</p>
-        <button type="submit" className="btn-sign-up" onClick={handleSignup}>Create your Amazon Account</button>
+          <div className="font-medium text-xl sm:text-2xl text-gray-800">Sign in</div>
+          <div className="mt-4">
+            <form>
+              <div className="flex flex-col mb-6">
+                <h5 className="font-semibold text-sm">Email</h5>
+                <input
+                  className="text-sm sm:text-base bg-transparent  pr-4 rounded-lg border border-gray-400 w-full py-1 focus:outline-none focus:outline-cyan-300 focus-visible:ring focus-visible:ring-cyan-300"
+                  type="email"
+                  value={email}
+                  required
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                <h5 className="font-semibold text-sm pt-4">Password</h5>
+                <input
+                  className="text-sm sm:text-base bg-transparent  pr-4 rounded-lg border border-gray-400 w-full py-1 focus:outline-none focus:outline-cyan-300 focus-visible:ring focus-visible:ring-cyan-300"
+                  type="password"
+                  value={password}
+                  required
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <button
+                  className="flex mt-6 items-center justify-center focus:outline-none text-black shadow text-sm sm:text-base bg-amber-300 hover:bg-amber-400 rounded py-1 w-full transition duration-150 ease-in"
+                  type="submit"
+                  onClick={handleLogin}
+                >
+                  Sign in
+                </button>
+              </div>
+            </form>
+            <p className="text-xs pt-2 pb-6">By continuing, you agree to FAKE AMAZON CLONE Conditions of Use and Privacy Notice.</p>
+          </div>
+        </div>
+        <div className="grid grid-cols-3 mt-4 gap-2 flex items-center">
+          <div className="col-span-1">
+            <div className="h-[1px] bg-gray-300" />
+          </div>
+          <div className="col-span-1">
+            <div className="text-xs text-gray-600">New to Amazon?</div>
+          </div>
+          <div className="col-span-1">
+            <div className="h-[1px] bg-gray-300" />
+          </div>
+        </div>
+        <div className="pb-8 w-full max-w-sm">
+          <button
+            type="submit"
+            className="flex mt-4 items-center justify-center focus:outline-none text-black shadow text-sm  bg-transparent border border-gray-300 hover:bg-slate-100 rounded py-1 w-full max-w-sm transition duration-150 ease-in"
+            onClick={handleSignup}
+          >
+            Create your Amazon Account
+          </button>
+        </div>
       </div>
-    </div>
+      <p className="text-center text-xs pt-4">© 1996-2024, Fake Amazon.com, Inc. or its affiliates</p>
+
+    </>
   );
 };
 export default Login;
